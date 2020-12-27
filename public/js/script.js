1eeff84e036f77dbdd3f94fa8857a2269c08fa45
André Nascimento <andrevsn1@gmail.com>
2020-12-27 19:17:03 -0300

    finish frontend

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,18 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarItems = document.getElementsByClassName("navbar-items")[0];
+const searchBar = document.getElementById("searchBar");
 
 toggleButton.addEventListener("click", () => {
   navbarItems.classList.toggle("active");
 });
 
 document.querySelector("footer").append(`© ${new Date().getFullYear()} `);
+
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value;
+  // const filteredPosts = posts.filter((post: any) => {
+  //   return post.title.contain(searchString) || post.content.contain(searchString) || post.category.contain(searchString);
+  // });
+  console.log(e.target.value);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
+import _ from "lodash";
 
 const app = express();
 
@@ -11,10 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let posts: Array<{}> = [];
 
 interface Post {
-  id: number,
   title: string,
   content: string,
   category: string,
+  kebabTitle: string,
 }
 
 //// ROUTES ////
@@ -26,16 +27,24 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/posts/:postName", (req, res) => {
+  posts.forEach((post: any) => {
+    if (_.kebabCase(post.title) === _.kebabCase(req.params.postName)) {
+      res.render("post", {title: post.title, content: post.content, category: post.category});
+    };
+  });
+});
+
 app.route("/compose")
   .get((req, res) => {
     res.render("compose");
   })
   .post((req, res) => {
     const post: Post = {
-      id: posts.length + 1,
       title: req.body.postTitle,
       content: req.body.postContent,
-      category: req.body.postCategory
+      category: req.body.postCategory,
+      kebabTitle: _.kebabCase(req.body.postTitle)
     }
     if ((post.category.length !==0) && (post.content.length !== 0) && (post.category.length !== 0)) {
       posts.push(post);
